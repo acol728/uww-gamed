2b90c213e9f0fb7774def870471839503e554ca2
Adam <acollett728@gmail.com>
2020-09-23 00:28:51 -0500

    Progres

--- js/index.js
+++ js/index.js
@@ -1,17 +1,20 @@
 //General functionality
 
+//Waits for document elements to be loaded before proceeding
 window.onload = function () {
-    var navbar = document.getElementById("navigation");
-    var sticky = navbar.offsetTop;
+    var navigation = document.getElementById("navigation");
+    var position = navigation.offsetTop;
 
+    //Function that makes navigation bar stick to the top of the page after scrolling down, if needed
     function addSticky() {
-        if (window.pageYOffset >= sticky) {
-            navbar.classList.add("sticky");
+        if (window.pageYOffset >= position) {
+            navigation.classList.add("sticky");
         } else {
-            navbar.classList.remove("sticky");
+            navigation.classList.remove("sticky");
         }
     }
 
+    //Calls addSticky() everytime user scrolls
     window.onscroll = function () {
         addSticky()
     };
